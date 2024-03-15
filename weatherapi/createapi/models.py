2fdba5750b191db286379f561a5cde5e0986ef1f
boburcoders <boburtoshniyozov4@gmail.com>
2024-03-15 14:17:04 +0500

    Weather App

---
+++ weatherapi/createapi/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class WeatherData(models.Model):
+    id = models.AutoField(primary_key=True)
+    city_name = models.CharField(max_length=30)
+    pressure = models.FloatField()
+    temperature = models.FloatField()
+    description = models.CharField(max_length=40)
+    icon = models.CharField(max_length=10)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.updated
+
+
